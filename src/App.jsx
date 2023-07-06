142ed1464e53de55165c1f46213f6690a38db092
Chris Sampson <csamposn@gmail.com>
2023-07-06 14:32:09 -0400

    Adding Websocket

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import './App.css';
+import "./App.css";
 
-import useLocalStorage from './lib/useLocalStorage';
-import useWindow from './lib/useWindow';
+import useLocalStorage from "./lib/useLocalStorage";
+import useWindow from "./lib/useWindow";
 
-import {useState} from 'react';
+import { useState } from "react";
 
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
-import {ThemeProvider} from '@emotion/react';
-import {theme} from './lib/theme';
-import {useTheme} from '@emotion/react';
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./lib/theme";
+import { useTheme } from "@emotion/react";
 
-import Navbar from './components/Navbar';
-import AppArea from './components/AppArea';
-import Settings from './components/Settings';
+import Navbar from "./components/Navbar";
+import AppArea from "./components/AppArea";
+import Settings from "./components/Settings";
 
-import defaultColors from './lib/defaultNoteColors';
+import defaultColors from "./lib/defaultNoteColors";
+
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", (ws) => {
+    console.log("up");
+});
+
+socket.on("disconnect", (ws) => {
+    console.log("down");
+});
+
+socket.on("error", (err) => {
+    console.error("Socket.IO Error", err);
+});
+
+socket.on("echo", (message) => {
+    console.log("echo", message);
+});
 
 const borderOffset = 16;
 
@@ -25,47 +45,59 @@ const AppContainer = styled.main`
     color: ${() => useTheme().text};
     transition: 0.5s;
     transition-property: background, color;
-  `
+`;
 
 function App() {
-  
-  const [themeMode, setThemeMode] = useLocalStorage('theme', 'dark');
+    const [themeMode, setThemeMode] = useLocalStorage("theme", "dark");
 
-  const [showSettings, setShowSettings] = useState(false);
+    const [showSettings, setShowSettings] = useState(false);
 
-  const [colors, setColors] = useLocalStorage('noteColors', defaultColors);
+    const [colors, setColors] = useLocalStorage("noteColors", defaultColors);
 
-  // auto adjust to window size 
-  const size = useWindow();
+    // auto adjust to window size
+    const size = useWindow();
 
-  const heightAfterOffset = size.height - borderOffset;
-  const widthAfterOffset = size.width - borderOffset;
+    const heightAfterOffset = size.height - borderOffset;
+    const widthAfterOffset = size.width - borderOffset;
 
-  function changeTheme() {
-    setThemeMode(themeMode === 'dark' ? 'light' : 'dark');
-  }
+    function changeTheme() {
+        setThemeMode(themeMode === "dark" ? "light" : "dark");
+    }
 
-  function toggleSettings() {
-    setShowSettings(!showSettings);
-  }
+    function toggleSettings() {
+        setShowSettings(!showSettings);
+    }
 
-  const windowRef = {
-    top: 0,
-    left: 0,
-    right: widthAfterOffset,
-    bottom: heightAfterOffset
-  }
+    const windowRef = {
+        top: 0,
+        left: 0,
+        right: widthAfterOffset,
+        bottom: heightAfterOffset,
+    };
 
-
-  return (
-    <ThemeProvider theme={theme[themeMode]}>
-      <AppContainer style={{height: heightAfterOffset, width: widthAfterOffset}} className="App">
-          <Navbar changeTheme={changeTheme} themeMode={themeMode} toggleSettings={toggleSettings} />
-        <AppArea  windowRef={windowRef} noteColors={colors} />
-      </AppContainer>
-      <Settings display={showSettings} setDisplay={setShowSettings} noteColors={colors} setColors={setColors} themeMode={themeMode} changeTheme={changeTheme} />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme[themeMode]}>
+            <AppContainer
+                style={{ height: heightAfterOffset, width: widthAfterOffset }}
+                className="App"
+            >
+                <Navbar
+                    changeTheme={changeTheme}
+                    themeMode={themeMode}
+                    toggleSettings={toggleSettings}
+                />
+                <AppArea windowRef={windowRef} noteColors={colors} />
+            </AppContainer>
+            <Settings
+                display={showSettings}
+                setDisplay={setShowSettings}
+                noteColors={colors}
+                setColors={setColors}
+                themeMode={themeMode}
+                changeTheme={changeTheme}
+            />
+        </ThemeProvider>
+    );
 }
 
 export default App;
